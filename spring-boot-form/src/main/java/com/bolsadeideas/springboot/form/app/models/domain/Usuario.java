7fd6843156ac9b1b899d0f1b374436c26c4abfb6
Gilberto <Betho6990@gmail.com>
2022-06-21 23:58:46 -0500

    Date validation with @NotNull and formatting with @DateTimeFormat

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+
+import java.util.Date;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -8,6 +11,8 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 //import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.bolsadeideas.springboot.form.app.validation.IdentificadorRegex;
 import com.bolsadeideas.springboot.form.app.validation.Requerido;
@@ -45,6 +50,10 @@ public class Usuario {
 	@Min(5)
 	@Max(5000)
 	private Integer cuenta;
+	
+	@NotNull
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date fechaNacimiento;
 
 	public String getUsername() {
 		return username;
@@ -102,5 +111,12 @@ public class Usuario {
 		this.cuenta = cuenta;
 	}
 
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
 	
 }
