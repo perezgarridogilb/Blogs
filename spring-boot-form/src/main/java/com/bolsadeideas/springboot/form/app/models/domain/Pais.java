aace79f16dfa9449b43aa00b20941419d34a0cd5
Gilberto <betho6990@gmail.com>
2022-07-05 23:32:54 -0500

    Filling select list with objects of the Country class

---
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Pais.java
@@ -0,0 +1,48 @@
+package com.bolsadeideas.springboot.form.app.models.domain;
+
+public class Pais {
+
+	private Integer id;
+	private String codigo;
+	private String nombre;
+
+	/**
+	 * Dos constructores, uno con parámetros y el otro sin parámetros
+	 */
+	public Pais() {
+		/* super(); */
+		// TODO Auto-generated constructor stub
+	}
+
+	public Pais(Integer id, String codigo, String nombre) {
+		/* super(); */
+		this.id = id;
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
