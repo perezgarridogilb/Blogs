371e782b4f092c4cd38b22fee6164c8dfe8dc330
Gilberto <Betho6990@gmail.com>
2023-06-15 00:04:47 -0600

    Profile

--- spring-boot-web1/src/main/java/com/bolsadeideas/springboot/web/app1/controllers/IndexController.java
+++ spring-boot-web1/src/main/java/com/bolsadeideas/springboot/web/app1/controllers/IndexController.java
@@ -1,15 +1,47 @@
 package com.bolsadeideas.springboot.web.app1.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bolsadeideas.springboot.web.app1.models.Usuario;
+
+
+
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index", "/", "/home"})
-	public String index() {
+	@GetMapping({"/index", "/", "", "/home"})
+	public String index(Map<String, Object> map) {
+		map.put("titulo", "Hola spring framework");
 		return "index";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(new Usuario());
+model.addAttribute("titulo", "Listado de usuarios");
+model.addAttribute("usuarios", usuarios);
+		return "listar";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Andrés");
+		usuario.setApellido("Apellido");
+		usuario.setEmail("andres@correo.com");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: " . concat(usuario.getNombre()));
+		return "perfil";
 	}
 }
---
+++ spring-boot-web1/src/main/java/com/bolsadeideas/springboot/web/app1/models/Usuario.java
@@ -0,0 +1,39 @@
+package com.bolsadeideas.springboot.web.app1.models;
+
+public class Usuario {
+	private String nombre;
+	private String apellido;
+	private String email;
+	
+	public Usuario (String nombre, String apellido) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
